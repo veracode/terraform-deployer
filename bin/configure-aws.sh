7d9575d3f9655307ef2e7a6fac0e6b95d7a2454f
Paul Lussier <pllsaph@gmail.com>
2019-02-07 14:40:27 -0500

    Set up profile based on account alias

--- bin/configure-aws.sh
+++ bin/configure-aws.sh
@@ -4,13 +4,17 @@
 
 mkdir ~/.aws
 
+CREDS="AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
+
+PROFILE=$($CREDS aws iam list-account-aliases --output text | awk '{print $2}')
+
 cat >~/.aws/config <<HERE
 [default]
 region = us-east-1
 HERE
 
 cat >~/.aws/credentials <<HERE
-[default]
+[$PROFILE]
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 
